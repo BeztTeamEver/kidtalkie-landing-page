7c11089e20ca11e2176c6ebd803d2daaba75c4a8
DESKTOP-DGQAMI1\Admin <giathanh.houang12344@gmail.com>
2024-01-28 10:46:22 +0700

    update form register early

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/route';
-
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
 ReactDOM.createRoot(document.getElementById('root')!).render(
+
 	<React.StrictMode>
+		<ToastContainer
+			position="top-right"
+			autoClose={3000}
+			hideProgressBar={false}
+			newestOnTop={false}
+			closeOnClick
+			rtl={false}
+			pauseOnFocusLoss
+			draggable
+			pauseOnHover
+		/>
 		<BrowserRouter>
 			<AppRoutes />
 		</BrowserRouter>
